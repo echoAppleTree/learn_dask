e4d82ac364e3e0908a8f0983436d80f022ec41f3
David-Alexandre <david.a.guenette@outlook.com>
2021-01-21 23:31:30 -0500

    Data Cleaning: Part 1 - Missing Values. Completed

--- scripts/library/dask_library/dask_dc.py
+++ scripts/library/dask_library/dask_dc.py
@@ -9,37 +9,72 @@ from dask.diagnostics import ProgressBar
 from matplotlib import pyplot as pyplot
 
 
-
-class Cleaning():
-
+class Cleaner():
+    
     def __init__(self, dataframe):
         self.dataframe = dataframe
+        
+    
+    # --- Columns' Methods --- #
+    
+    def select_columns(self, columns):
+        """ Select specific column(s). """
 
+        filtered_df = self.dataframe[columns]
+        return filtered_df
+    
+    def drop_columns(self, columns):
+        """ Drop specific column(s). """
 
+        dropped_df = self.dataframe.drop(columns, axis=1)
+        return dropped_df
+    
+    def rename_columns(self, columns):
+        """ Rename specific column(s) """
 
-    def calc_miss_val_percent(self):
-        """
-        - Counting missing values in the DataFrame
-        - Calculating the percent of missing values in the DataFrame
-        - Computing the DAG
-        """
+        renamed_df = self.dataframe.rename(columns=columns)
+        return renamed_df
+    
+    # --- Rows' Methods --- #
+    
+    def select_rows(self, r0, r1):
+        """ Select a specific amount of rows. """
 
+        filtered_df = self.dataframe.loc[r0:r1]
+        return filtered_df
+    
+    
+    # --- Missing Values' Methods --- #
+    
+    # Cleaning - Stage 0
+    def calc_pct_missing_val(self):
+        """ Calculate the percentage of missing values for each columns. """
+        
         missing_values = self.dataframe.isnull().sum()
-        missing_count = ((missing_values / dataframe.index.size) * 100)
+        percent_missing = ((missing_values / self.dataframe.index.size) * 100)
+        return percent_missing
+    
+    # Cleaning - Stage 1
+    def drop_columns_treshold(self, treshold, ls):
+        """ Drop columns that have a certain treshold of missing values. """
 
-        with ProgressBar():
-            missing_count_pct = missing_count.compute()
+        columns_to_drop = list(ls[ls >= treshold].index)
+        df_clean_stage01 = self.dataframe.drop(columns_to_drop, axis=1)
+        return df_clean_stage01
 
-        return missing_count_pct
+    # Cleaning - Stage 2    
+    def drop_rows_treshold(self, treshold, ls):
+        """ Drop rows that have a certain treshold of missing values"""
 
+        rows_to_drop = list(ls[(ls > 0) & (ls < treshold)].index)
+        df_clean_stage02 = self.dataframe.dropna(subset=rows_to_drop)
+        return df_clean_stage02
+    
+    # Cleaning - Stage 3
+    def imputing_columns_missing_val(self, treshold ,ls, col_name):
+        """ Impute default values in remaining low missing values columns"""
 
-
-
-    def filter_sparse_columns(self, missing_count_pct, treshold):
-        """Filtering sparse columns"""
-
-        columns_to_drop = missing_count_pct[missing_count_pct > treshold].index
-        with ProgressBar():
-            df_dropped = self.dataframe.drop(columns_to_drop, axis=1).persist()
-
-        return df_dropped    
+        remaining_columns_to_clean = list(ls[(ls > 0) & (ls < treshold)].index)
+        unknown_default_dict = dict(map(lambda columnName: (columnName, col_name), remaining_columns_to_clean))
+        df_clean_stage03 = self.dataframe.fillna(unknown_default_dict)
+        return df_clean_stage03
